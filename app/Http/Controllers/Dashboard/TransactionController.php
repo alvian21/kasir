8d99921f73229299e9823fc39e61441722fbc2aa
alvian <alvian.ardhiansyah@gmail.com>
2019-12-19 22:40:18 +0700

    kasir

--- app/Http/Controllers/Dashboard/TransactionController.php
+++ app/Http/Controllers/Dashboard/TransactionController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Receipt;
 
 class TransactionController extends Controller
 {
     public function index()
     {
-        return view('dashboard.transaksi.index');
+        $data = Receipt::all();
+
+        foreach($data as $row){
+            $row = json_decode($row->name);
+
+            // dd($row);
+        }
+        return view('dashboard.transaksi.index',['data'=>$data]);
     }
 }
