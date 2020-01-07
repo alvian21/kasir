2731e5a958754118edb7d62a9da1bead100768f2
alvian <alvian.ardhiansyah@gmail.com>
2020-01-07 22:04:28 +0700

    kasir

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -88,12 +88,12 @@
       <div class="container">
         <div class="row align-items-center justify-content-xl-between">
           <div class="col-xl-6">
-            <div class="copyright text-center text-xl-left text-muted">
-              © 2018 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
-            </div>
+            {{-- <div class="copyright text-center text-xl-left text-muted">
+              © 2018 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank"></a>
+            </div> --}}
           </div>
           <div class="col-xl-6">
-            <ul class="nav nav-footer justify-content-center justify-content-xl-end">
+            {{-- <ul class="nav nav-footer justify-content-center justify-content-xl-end">
               <li class="nav-item">
                 <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
               </li>
@@ -106,7 +106,7 @@
               <li class="nav-item">
                 <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
               </li>
-            </ul>
+            </ul> --}}
           </div>
         </div>
       </div>
