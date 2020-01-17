5b4368b08005ad66e43e57429dea9138afcd3507
alvian <alvian.ardhiansyah@gmail.com>
2020-01-17 21:54:31 +0700

    revisi

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -2,13 +2,14 @@
 
 @section('header')
 <div class="row">
+    @if(Auth()->user()->role == 'karyawan')
     <div class="col-xl-3 col-lg-6">
       <div class="card card-stats mb-4 mb-xl-0">
         <div class="card-body">
           <div class="row">
             <div class="col">
-              <h5 class="card-title text-uppercase text-muted mb-0">Traffic</h5>
-              <span class="h2 font-weight-bold mb-0">350,897</span>
+              <h6 class="card-title text-uppercase text-muted mb-0">Barang Terjual</h6>
+            <span class="h2 font-weight-bold mb-0">{{$hasil}}</span>
             </div>
             <div class="col-auto">
               <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
@@ -16,20 +17,19 @@
               </div>
             </div>
           </div>
-          <p class="mt-3 mb-0 text-muted text-sm">
-            <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-            <span class="text-nowrap">Since last month</span>
-          </p>
+
         </div>
       </div>
     </div>
+    @endif
+
     <div class="col-xl-3 col-lg-6">
       <div class="card card-stats mb-4 mb-xl-0">
         <div class="card-body">
           <div class="row">
             <div class="col">
-              <h5 class="card-title text-uppercase text-muted mb-0">New users</h5>
-              <span class="h2 font-weight-bold mb-0">2,356</span>
+              <h5 class="card-title text-uppercase text-muted mb-0">  @if(Auth()->user()->role == 'karyawan')Pemasukan @else Penghasilan @endif</h5>
+              <span class="h2 font-weight-bold mb-0">Rp {{ $pemasukan }}</span>
             </div>
             <div class="col-auto">
               <div class="icon icon-shape bg-warning text-white rounded-circle shadow">
@@ -37,14 +37,12 @@
               </div>
             </div>
           </div>
-          <p class="mt-3 mb-0 text-muted text-sm">
-            <span class="text-danger mr-2"><i class="fas fa-arrow-down"></i> 3.48%</span>
-            <span class="text-nowrap">Since last week</span>
-          </p>
+
         </div>
       </div>
     </div>
-    <div class="col-xl-3 col-lg-6">
+
+    {{-- <div class="col-xl-3 col-lg-6">
       <div class="card card-stats mb-4 mb-xl-0">
         <div class="card-body">
           <div class="row">
@@ -85,13 +83,13 @@
           </p>
         </div>
       </div>
-    </div>
+    </div> --}}
 </div>
 
 @endsection
 
 @section('content')
-<div class="row">
+{{-- <div class="row">
     <div class="col-xl-8 mb-5 mb-xl-0">
       <div class="card bg-gradient-default shadow">
         <div class="card-header bg-transparent">
@@ -145,6 +143,6 @@
         </div>
       </div>
     </div>
-</div>
+</div> --}}
 
 @endsection
